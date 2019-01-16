cba6623637922f4127498cda2cb60a9b9e650abd
TimerChen <knosikay@gmail.com>
2019-01-17 03:26:42 +0800

    download renew

--- datasets/download.bash
+++ datasets/download.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir pics
-mkdir minipics
+mkdir pics valpics
+mkdir minipics minivalpics
 sudo apt install ttf-dejavu
 wget http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz
 tar -xzf aclImdb_v1.tar.gz
